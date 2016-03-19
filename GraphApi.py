8391e92744dc264bc402c61a80f0b1bdc0762bfe
HARWANI <hharwani@amazon.com>
2016-03-19 16:43:04 -0700

    add comments and change README

--- GraphApi.py
+++ GraphApi.py
@@ -1,23 +1,24 @@
-"""
-A simple example script to get all posts on a user's timeline.
-Originally created by Mitchell Stewart.
-<https://gist.github.com/mylsb/10294040>
-"""
 import facebook
 import requests
 
 
-#get your access token from here:
-access_token = 'CAACEdEose0cBAPZAXZC4oLGfMrACtj5fIRlMb2nhGaBCZBaxIRApRsaPJbFujzbnHlnc2D3LEevqCgPRg6ZBMSF2PbIWTkdHZCa6r3sQGRxb2ISc6VSspkMsnSZChT0xuA5X0MxkmDbuRZCncKuM2C9H0cVnPZCapQWHCrvCTUB39vhp8pIkHvmYmVn7Fm0MLLEDBPtNKHPwFVs513RVHVnUrRtkQffKPikZD'
-pic = '10205355984653648/likes'
+#get your access token from here: https://developers.facebook.com/tools/explorer
+access_token = 'CAACEdEose0cBACy3vnaBT071Lup3mn4kH8NauM4XHeyY1hMByDAvLlb2W4EUSsZBBg880XRKY00ZA9xmkDP6RXbZAEZA59pzJLGKxcGp48ZAgcMTcbM7ZC4K4Os8qFdWXGQdqiMUx2wu4bJZAOcwwvo4CwKcp3DraWFCylVYaOXb7d8WHiFbZBPPpgNJU6VrIK5JpyNtjyFAaEMC21iRFu9g0ZBZCUKYaUuhkZD'
 
+#https://www.facebook.com/photo.php?fbid=10201714680143311&set=pb.1669607551.-2207520000.1458430750.&type=3&theater
+#when I say replace the id of the pic, I mean fbId of any of the pic as shown in the above url.
 
+picId = '10201714680143311/likes'
+
+#extracting the first names from the returned JSON
 def processNames(names):
     fname=[]
     for i in names:
         fname.append(i["name"].split()[0])
     return fname
 
+
+#calling the API and writing the results to the File
 def writeToFile():
     femaleLikes=[]
     text_file = open("Output.txt", "w")
@@ -25,12 +26,16 @@ def writeToFile():
         r = requests.get("http://api.genderize.io/?name="+fname)
         result=r.json()
         femaleLikes.append(result["gender"])
-        text_file.write("%s\n" % result["gender"])
-    text_file.write("%s\n" % femaleLikes.count("female") )
+        text_file.write("The gender is: %s\n" % result["gender"])
+    text_file.write("The total number of female likes is: %s\n" % femaleLikes.count("female") )
 
 
+
+#doing all the initializations
 graph = facebook.GraphAPI(access_token)
-picData = graph.get_object(pic)
+picData = graph.get_object(picId)
+
+#As the data is returned in forms of pagination, need to retrieve rest by pulling in next pages.
 names=[]
 while True:
     try:
@@ -39,7 +44,13 @@ while True:
     except KeyError:
         break
 
+#processing the data retrieved by querying
 fnames=processNames(names)
-print len(fnames)
+
+print "Teh total number of likes on the pic is: ", len(fnames)
+
+#writing to file
 writeToFile()
+
+print "Execution completed, see the results in Output.txt file."
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FacebookPicLikesByGirls
-This is a simple python script which calculates the number of female likes on a particular pic
+This is a simple python script which calculates the number of female likes on a particular pic.
+
+See the GraphApi.py file for instructions on how to change the parameters. The script is slow for now, as there are expensive API calls. I will be optimizing this soon.
+
+
+Every like is analyzed and a gender corresponding to the file is written to the file. The output file is Output.txt
+
+
+Not tested the failure scenarios like what happens if the pic has no likes. Willdo that soon.
