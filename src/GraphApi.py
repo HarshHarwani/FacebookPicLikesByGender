4dd4116f11e157ac28c083eecd86eb431d253da5
HARWANI <hharwani@amazon.com>
2016-03-20 00:40:11 -0700

    refactor code

--- src/GraphApi.py
+++ src/GraphApi.py
@@ -4,8 +4,12 @@ import facebook
 # get your access token from here: https://developers.facebook.com/tools/explorer
 access_token = 'CAACEdEose0cBAGs2ANb6E2VOTJmz32u7ZByJEGsyXEL71VUbcu4KjOvYdpzM60lurYEhffCZCgtk0ZBhs6I8iFkIdmtZAQy9HdGrcIjndZAYtWGCYortiSOoZAjLSoHxBQiHxAheqjZBMSuoAZB2WFNPSdKba6KU7dX0cjE03wIuUkgyf1pOnTeR13ut4Fzw41sKYfpgqkEPgjpuXto8UxS8PzhmfZAd6TsYZD'
 
+# create an account on https://gender-api.com and append your server key below.
+ApiKey = 'ZJHjMWWHLZyVhSeJRT'
+
 # https://www.facebook.com/photo.php?fbid=10205355984653648&set=pb.1669607551.-2207520000.1458430750.&type=3&theater
 # when I say replace the id of the pic, I mean fbId of any of the pic as shown in the above url.
+
 # List of picIds of the most recent pics
 picIDList = ['10205355984653648', '10204664229920212', '10201859340079719']
 text_file = open("Output.txt", "w")
@@ -27,7 +31,7 @@ def processNames(names):
 def writeToFile(count, resultList):
     genderList = []
     for fname in fnames:
-        r = requests.get("https://gender-api.com/get?name="+fname+"&key=ZJHjMWWHLZyVhSeJRT")
+        r = requests.get("https://gender-api.com/get?name=" + fname + "&key=" + ApiKey)
         result = r.json()
         genderList.append(result["gender"])
     femaleCount = float(genderList.count("female"))
@@ -58,6 +62,9 @@ for picId in picIDList:
     print "Teh total number of likes on the pic is: ", len(fnames)
     writeToFile(count, resultList)
 
+#calculating average of the female likes
 average = sum(resultList, 0.0) / len(resultList)
+
 text_file.write("Average percentage of female likes: %s\n" % average)
+
 print "Execution completed, see the results in Output.txt file."
